a777beb1cf68e600d476fa20d43706c9fa87d518
JoshCobra <hulls.reach-0p@icloud.com>
2025-08-14 13:42:46 -0600

    Update Song component to include imgSrc prop and enhance styling in App.css

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Header from './components/header';
 import Song from './components/song';
 
 class App extends Component {
-
-  componentDidMount() {
-    console.log("App Fully Loaded")
-  }
   
   render() {
     return (
       <div className="App">
         <Header/> 
-        <Song songTitle= "Romance" songArtist="Hiroshi Suzuki"/>
-        <Song songTitle= "Siempre Hay Esperanza" songArtist= "Sade"/>
-        <Song songTitle= "London Beach" songArtist= "Veeko Morlet"/>
+        <Song songTitle= "Romance" songArtist="Hiroshi Suzuki" imgSrc="https://i.scdn.co/image/ab67616d0000b273de1acb1009d8ddbeccb2889a" />
+        <Song songTitle= "Siempre Hay Esperanza" songArtist= "Sade" imgSrc="https://i.scdn.co/image/ab67616d0000b2738d6c05fb48d5834d425128bf" />
+        <Song songTitle= "London Beach" songArtist= "Veeko Morlet" imgSrc="https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/44/3d/ba/443dba81-e16e-9a03-8795-027d435e990b/859793818282_cover.jpg/1200x630bb.jpg" />
       </div>
     );
   }
+
+  componentDidMount() {
+  console.log("App Fully Loaded")
+  }
+
 }
 
 export default App;
--- src/components/song.js
+++ src/components/song.js
@@ -4,10 +4,10 @@ import { Component } from "react";
 class Song extends Component {
     render() {
         const { songTitle, songArtist } = this.props;
-        
+
         return(
             <div className="songContainer">
-                <img src=""/>
+                <img src={this.props.imgSrc} alt={songTitle}/>
                 <div className="songAttributes">
                     <h4 className="songTitle">{songTitle}</h4>
                     <p className="songArtist">{songArtist}</p>
