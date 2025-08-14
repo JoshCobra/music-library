a6771c194da2c63e235b57224848b29bf57bd13c
JoshCobra <hulls.reach-0p@icloud.com>
2025-08-14 13:30:10 -0600

    Refactor App component to use class syntax and add componentDidMount lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Song from './components/song';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/> 
-      <Song songTitle= "Romance" songArtist="Hiroshi Suzuki"/>
-      <Song songTitle= "Siempre Hay Esperanza" songArtist= "Sade"/>
-      <Song songTitle= "London Beach" songArtist= "Veeko Morlet"/>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    console.log("App Fully Loaded")
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header/> 
+        <Song songTitle= "Romance" songArtist="Hiroshi Suzuki"/>
+        <Song songTitle= "Siempre Hay Esperanza" songArtist= "Sade"/>
+        <Song songTitle= "London Beach" songArtist= "Veeko Morlet"/>
+      </div>
+    );
+  }
 }
 
 export default App;
